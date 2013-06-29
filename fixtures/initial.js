4eee6a6a2d4c8163c058701af080c71cdf5bb919
Pepe Cano <ppcanodehuelva@gmail.com>
2013-06-29 13:41:52 +0200

    Add category slug

--- fixtures/initial.js
+++ fixtures/initial.js
@@ -1,18 +1,22 @@
 
 Th.Event.FIXTURES = [
   {id: 1, title: "Porta Fidei", content: "Lugar: Monasterio de Santa Clara de Moguer Información: 959 372 193 <strong>Moguer</strong>", categories: [1]},
-  {id: 2, title: "Porta Fidei 2", content: "Lugar: Monasterio de Santa Clara de Moguer Información: 959 372 193 <strong>Moguer</strong>"},
-  {id: 3, title: "Porta Fidei 3", content: "Lugar: Monasterio de Santa Clara de Moguer Información: 959 372 193 <strong>Moguer</strong>"}
+  {id: 2, title: "Porta Fidei 2", content: "Lugar: Monasterio de Santa Clara de Moguer Información: 959 372 193 <strong>Moguer</strong>", categories: [2]},
+  {id: 3, title: "Porta Fidei 3", content: "Lugar: Monasterio de Santa Clara de Moguer Información: 959 372 193 <strong>Moguer</strong>", categories: [3]}
 ];
 
 
 Th.Category.FIXTURES = [
-  {id: 1, name: "Exposicion"}, 
-  {id: 2, name: "Ninos"}, 
-  {id: 3, name: "Disco"}, 
-  {id: 4, name: "Hi.."}, 
-  {id: 5, name: "Naturaleza"}, 
-  {id: 6, name: "Play"}
+  {id: 1, name: "Cine", slug: "cine"}, 
+  {id: 2, name: "Deportes-Competiciones", slug: "deporte"}, 
+  {id: 3, name: "Exposiciones", slug: "exposicion"}, 
+  {id: 4, name: "Fiestas-Tradiciones", slug: "tradicion"}, 
+  {id: 5, name: "Mercado", slug: "mercado"}, 
+  {id: 6, name: "Infantil", slug: "infantil"}, 
+  {id: 7, name: "Literatura-Conferencias-Charlas", slug: "literatura"}, 
+  {id: 8, name: "Música-Festivales-Flamencos", slug: "musica"}, 
+  {id: 9, name: "Naturaleza-Rutas", slug: "naturaleza"}, 
+  {id: 10, name: "Taller-Formación-Cursos", slug: "taller"}, 
+  {id: 11, name: "Teatro-Danza-Humor", slug: "teatro"}, 
+  {id: 12, name: "Visitas", slug: "visitas"}
 ];
-
-
