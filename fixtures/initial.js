c283bd876a5485344e42ac7b7ab792dbc112e282
Pepe Cano <ppcanodehuelva@gmail.com>
2013-10-09 21:12:04 +0200

    Add place logic

--- fixtures/initial.js
+++ fixtures/initial.js
@@ -1,7 +1,7 @@
 
 Th.Event.FIXTURES = [
-  {id: 1, title: "Porta Fidei", content: "Lugar: Monasterio de Santa Clara de Moguer Información: 959 372 193 <strong>Moguer</strong>", categories: [1]},
-  {id: 2, title: "Porta Fidei 2", content: "Lugar: Monasterio de Santa Clara de Moguer Información: 959 372 193 <strong>Moguer</strong>", categories: [2]},
+  {id: 1, title: "Porta Fidei", content: "Lugar: Monasterio de Santa Clara de Moguer Información: 959 372 193 <strong>Moguer</strong>", categories: [1, 2]},
+  {id: 2, title: "Porta Fidei 2", content: "Lugar: Monasterio de Santa Clara de Moguer Información: 959 372 193 <strong>Moguer</strong>", categories: [2, 3]},
   {id: 3, title: "Porta Fidei 3", content: "Lugar: Monasterio de Santa Clara de Moguer Información: 959 372 193 <strong>Moguer</strong>", categories: [3]}
 ];
 
@@ -18,5 +18,15 @@ Th.Category.FIXTURES = [
   {id: 9, name: "Naturaleza-Rutas", slug: "naturaleza"}, 
   {id: 10, name: "Taller-Formación-Cursos", slug: "taller"}, 
   {id: 11, name: "Teatro-Danza-Humor", slug: "teatro"}, 
-  {id: 12, name: "Visitas", slug: "visitas"}
+  {id: 12, name: "+Ocio", slug: "ocio"}
+];
+
+
+Th.Place.FIXTURES = [
+  {id: 1, name: "Ándevalo", slug: "andevalo"}, 
+  {id: 2, name: "Condado", slug: "condado"}, 
+  {id: 3, name: "Costa", slug: "costa"}, 
+  {id: 4, name: "Cuenca Minera", slug: "cuencaminera"}, 
+  {id: 5, name: "Huelva", slug: "huelva"},
+  {id: 6, name: "Sierra", slug: "sierra"}
 ];
